9f1778c6ede8f1ac6fa840c64f02578bd994cc6b
Shazam72 <jfrancoiszouba72@gmail.com>
2024-04-28 01:05:17 +0200

    generatePortfolios function

--- src/portfolios.ts
+++ src/portfolios.ts
@@ -27,65 +27,69 @@ const cylinderGeoParams = {
 const portfolios = new Group();
 const portfolioItems: Group[] = [];
 
-const portfolioInfos = await getPortfolioLinks();
-const length = portfolioInfos.length / xOffset;
-console.time();
 
-let cylinderGeo = new CylinderGeometry(
-  cylinderGeoParams.radius,
-  cylinderGeoParams.radius,
-  cylinderGeoParams.height,
-  cylinderGeoParams.radialSegments,
-  cylinderGeoParams.heightSegments,
-  cylinderGeoParams.openEnded,
-  cylinderGeoParams.thetaStart,
-  cylinderGeoParams.thetaLength
-);
-const btnGeo = new PlaneGeometry(3, 1.5);
-const btnMat = new MeshBasicMaterial({
-  color: 0xff0fff,
-  side: DoubleSide,
-  transparent: true,
-  opacity: 0.5,
-});
-for (let y = 0; y < length; y++) {
-  const portfolioItem = new Group();
-  for (let i = 0; i < xOffset; i++) {
-    const idx = y * 4 + i;
+const generatePortfolios = async ()=>{
+  console.time();
+  const portfolioInfos = await getPortfolioLinks();
+  const length = portfolioInfos.length / xOffset;
 
-    const material = new MeshBasicMaterial({
-      side: DoubleSide,
-    });
-
-    const cylinder = new Mesh(cylinderGeo, material);
-    const btn = new Mesh(btnGeo, btnMat);
-    btn.position.set(4.5, 0, 10.8);
-    btn.rotateY(Math.PI / xOffset);
-    const cylinderGroup = new Group();
-    cylinderGroup.userData.isCylinderGroup = true;
-    btn.visible = false;
-    cylinderGroup.add(cylinder, btn);
-    btn.userData.link = portfolioInfos[idx].link;
-    cylinder.userData.img = portfolioInfos[idx].img;
-    const texture = textureLoader.load(cylinder.userData.img);
-    texture.colorSpace = SRGBColorSpace;
-    texture.center.set(0.5, 0.5);
-    texture.flipY = false;
-    texture.rotation = Math.PI;
-    cylinder.material.map = texture;
-    cylinder.name = `item-${y}-${i}`;
-    cylinderGroup.rotation.y = (2 * (i * Math.PI)) / xOffset;
-    portfolioItem.add(cylinderGroup);
+  let cylinderGeo = new CylinderGeometry(
+    cylinderGeoParams.radius,
+    cylinderGeoParams.radius,
+    cylinderGeoParams.height,
+    cylinderGeoParams.radialSegments,
+    cylinderGeoParams.heightSegments,
+    cylinderGeoParams.openEnded,
+    cylinderGeoParams.thetaStart,
+    cylinderGeoParams.thetaLength
+  );
+  const btnGeo = new PlaneGeometry(3, 1.5);
+  const btnMat = new MeshBasicMaterial({
+    color: 0xff0fff,
+    side: DoubleSide,
+    transparent: true,
+    opacity: 0.5,
+  });
+  for (let y = 0; y < length; y++) {
+    const portfolioItem = new Group();
+    for (let i = 0; i < xOffset; i++) {
+      const idx = y * 4 + i;
+  
+      const material = new MeshBasicMaterial({
+        side: DoubleSide,
+      });
+  
+      const cylinder = new Mesh(cylinderGeo, material);
+      const btn = new Mesh(btnGeo, btnMat);
+      btn.position.set(4.5, 0, 10.8);
+      btn.rotateY(Math.PI / xOffset);
+      const cylinderGroup = new Group();
+      cylinderGroup.userData.isCylinderGroup = true;
+      btn.visible = false;
+      cylinderGroup.add(cylinder, btn);
+      btn.userData.link = portfolioInfos[idx].link;
+      cylinder.userData.img = portfolioInfos[idx].img;
+      const texture = textureLoader.load(cylinder.userData.img);
+      texture.colorSpace = SRGBColorSpace;
+      texture.center.set(0.5, 0.5);
+      texture.flipY = false;
+      texture.rotation = Math.PI;
+      cylinder.material.map = texture;
+      cylinder.name = `item-${y}-${i}`;
+      cylinderGroup.rotation.y = (2 * (i * Math.PI)) / xOffset;
+      portfolioItem.add(cylinderGroup);
+    }
+    portfolioItem.position.y =
+      y * (cylinderGeoParams.height + 0.3) -
+      (cylinderGeoParams.height * yOffset) / 2;
+    // portfolioItem.rotation.y = Math.PI / 3 + Math.PI * Math.random();
+    portfolios.add(portfolioItem);
+    portfolioItems.push(portfolioItem);
   }
-  portfolioItem.position.y =
-    y * (cylinderGeoParams.height + 0.3) -
-    (cylinderGeoParams.height * yOffset) / 2;
-  // portfolioItem.rotation.y = Math.PI / 3 + Math.PI * Math.random();
-  portfolios.add(portfolioItem);
-  portfolioItems.push(portfolioItem);
+  console.timeEnd();
 }
-console.timeEnd();
 
+generatePortfolios()
 const animate = () => {
   const elapsed = 0.0007;
   portfolioItems.forEach((portfolioItem, idx) => {
